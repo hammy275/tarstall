94d956a3b22802f8b56bce2d64b78cac24445e14
hammy275 <hammy275@gmail.com>
2026-09-05 10:23:11 -0400

    Add CLI Parsing

---
+++ src/cli.rs
@@ -0,0 +1,63 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(about)]
+pub struct TarstallCli {
+    #[arg(short, long, default_value_t = false)]
+    /// Enables verbose mode for this run of tarstall
+    pub verbose: bool,
+    #[command(subcommand)]
+    pub command: Command
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Install a program from a file path or URL
+    Install(InstallArgs),
+    /// Remove a program by name
+    Remove(ProgramArgs),
+    /// List all installed programs
+    List {},
+    /// Runs first-time setup
+    First {},
+    /// Removes tarstall from your system
+    Erase {},
+    /// Update tarstall and/or installed programs
+    Update(UpdateArgs),
+    /// Manage a program by name
+    Manage(ProgramArgs),
+    /// Remove tarstall's lock; only use if tarstall isn't already running
+    RemoveLock {},
+    /// Configure tarstall
+    Config {},
+
+}
+
+#[derive(Args, Debug)]
+pub struct InstallArgs {
+    /// The archive/directory/file/git URL/archive URL to install from
+    pub source: String,
+    #[arg(short, long)]
+    /// The name for the installed program
+    pub name: Option<String>
+}
+
+#[derive(Args, Debug)]
+pub struct ProgramArgs {
+    /// The name of the program
+    pub program: String
+}
+
+#[derive(Args, Debug)]
+pub struct UpdateArgs {
+    #[arg(short, long, default_value_t = false)]
+    /// Update tarstall
+    pub tarstall: bool,
+    #[arg(short, long)]
+    /// Update an installed program
+    pub program: Option<String>
+}
+
+pub fn get_args() -> TarstallCli {
+    TarstallCli::parse()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::env;
 use std::sync::Arc;
 use std::sync::mpsc::channel;
+use crate::cli::TarstallCli;
 use crate::task::{TaskRunner, Tasks};
 use crate::task::ProgressSender::Sender;
 use crate::tasks::file_transfer::{FileTransfer, TransferMode};
@@ -13,70 +15,12 @@ mod tasks;
 mod util;
 mod db;
 mod config;
-
-fn extra_get_tempdir() -> TempDir {
-    temp_dir().unwrap()
-}
-
+mod cli;
 fn main() {
-    // Basic file transfer
-    let task1 = FileTransfer {
-        src: "tarstall.exe".parse().unwrap(),
-        dst: "tarstall.exe2".parse().unwrap(),
-        transfer_mode: TransferMode::COPY
-    };
-    // Another basic file transfer
-    let task2 = FileTransfer {
-        src: "tarstall.exe".parse().unwrap(),
-        dst: "tarstall.exe3".parse().unwrap(),
-        transfer_mode: TransferMode::COPY
-    };
-    // A task of tasks
-    let sub_task1 = FileTransfer {
-        src: "tarstall.exe".parse().unwrap(),
-        dst: "tarstall.exe4".parse().unwrap(),
-        transfer_mode: TransferMode::COPY
-    };
-    let sub_task2 = FileTransfer {
-        src: "tarstall.exe".parse().unwrap(),
-        dst: "tarstall.exe5".parse().unwrap(),
-        transfer_mode: TransferMode::COPY
-    };
-    let mut sub_tasks: Tasks = Vec::new();
-    sub_tasks.push((Arc::new(sub_task1), 0.25));
-    sub_tasks.push((Arc::new(sub_task2), 0.75));
-    let task3 = TaskOfTasks {
-        tasks: sub_tasks,
-    };
-    // A folder transfer. Dangerous to unwrap(), but this is a temporary main for testing as I build this out.
-    let task4 = create_folder_transfer("incremental".into(), "incremental_copy".into(), TransferMode::COPY).unwrap();
-    
-    let mut tasks: Tasks = Vec::new();
-    tasks.push((Arc::new(task1), 1.0));
-    tasks.push((Arc::new(task2), 1.0));
-    tasks.push((Arc::new(task3), 2.0));
-    tasks.push((Arc::new(task4), 10.0));
-    let (sender, receiver) = channel();
-    let mut task_runner = TaskRunner::create(tasks, Sender(sender));
-    let run_tasks_handle = task_runner.run_tasks();
-    let mut progress = 0.0;
-    while progress < 1.0 {
-        match receiver.recv() {
-            Ok(amount) => {
-                progress = amount;
-                println!("Progress: {}", progress)
-            }
-            _ => break
-        }
+    if env::args_os().count() <= 1 {
+        // TODO: Print about
+    } else {
+        let cli = cli::get_args();
+        println!("{:?}", cli)
     }
-    match run_tasks_handle.join() {
-        Ok(task_result) => {
-            match task_result {
-                Ok(_) => println!("Done!"),
-                Err(msg) => println!("Error: {}", msg)
-            }
-        }
-        Err(_) => println!("Join error")
-    }
-    config::load().unwrap()
 }
