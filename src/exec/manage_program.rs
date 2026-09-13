43bb8937da6831340e26dbb1ef80afc1f39b8890
hammy275 <hammy275@gmail.com>
2026-09-12 22:23:57 -0400

    Finish Shortcut Creation

--- src/exec/manage_program.rs
+++ src/exec/manage_program.rs
@@ -1,6 +1,7 @@
 use std::env::consts::OS;
 use std::string::ToString;
 use directories::UserDirs;
+use lnks::Shortcut;
 use crate::args::ProgramArgs;
 use crate::config::{has_program, tarstall_home};
 use crate::task::TaskResult;
@@ -30,10 +31,17 @@ pub fn manage(args: &ProgramArgs, ui: &mut dyn UI) -> TaskResult {
 }
 
 fn windows_shortcut(args: &ProgramArgs, ui: &mut dyn UI) -> TaskResult {
+    // Note: Windows shortcuts are not tracked in tarstall's database since they're easily-visible
+    // files.
     match ui.ask_file(tarstall_home().join("bin").join(args.program.clone())) {
         Ok(path) => match UserDirs::new() {
             Some(dirs) => match dirs.desktop_dir() {
-                Some(desktop_dir) => todo!("Make shortcut"),
+                Some(desktop_dir) => {
+                    let shortcut = Shortcut::new(path);
+                    shortcut.save(desktop_dir.join(
+                            args.program.as_str()).with_extension("lnk"))
+                        .map_err(| err | { err.to_string() })
+                },
                 None => Err("Couldn't find desktop folder.".to_string())
             }
             None => Err("Couldn't find desktop folder.".to_string())
